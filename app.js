06ae77fa335c0cc7f486432d8634e8801eb04ac7
Nathan Smith <nathan@hebemedia.com>
2016-02-18 15:43:30 +0000

    Added some node packages Setup requires and middleware (CORS,body-parser etc) in app

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var config = require('nconf').argv().env().file({ file:  __dirname + '/config/env.json' });
+var JSONWebTokenMiddleware = require('express-jwt');
+var Auth0Client = require('auth0').ManagementClient;
+
+// ====================================================================
+// Body Parser
+// ====================================================================
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json({
+  type: function(req){
+    return true;
+  }
+}));
+
+// ====================================================================
+// CORS
+// ====================================================================
+var corsOptions = require('./app/cors');
+app.use(cors(corsOptions));
+
+
+
+
+
+
 
 app.get('/', function(req, res){
   res.send('Hello World');
